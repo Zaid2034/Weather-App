4f16ed18a6c2f66421e2617028c41fe6921587c2
Zaid2034 <mdzaidali2034@gmail.com>
2024-02-23 02:18:57 +0530

    weather app

---
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -0,0 +1,34 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object Constants {
+    const val APP_ID:String="42d2e87590c2ff83a55c6dff966b372a"
+    const val BASE_URL:String="http://api.openweathermap.org/data/"
+    const val METRIC_UNIT:String="metric"
+    const val PREFERENCE_NAME="WeatherAppPreference"
+    const val WEATHER_RESPONSE_DATA="weather_response_data"
+    fun isNetworkAvailable(context: Context): Boolean {
+        // It answers the queries about the state of network connectivity.
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network      = connectivityManager.activeNetwork ?: return false
+            val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            // Returns details about the currently active default data network.
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        }
+    }
+
+}
